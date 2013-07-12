0519ca6b623a72509626e6ddde39a8e9bd752349
kysoh <sohkamyung@gmail.com>
2013-07-12 09:01:13 +0800

    Added cdn.static-economist.com to AcceptDomainList

--- Magazine/Economist/wget-get-economist-weekly.sh
+++ Magazine/Economist/wget-get-economist-weekly.sh
@@ -13,6 +13,6 @@ rejectList="js,spacer.gif,black.gif,Spacer.gif,spn-printable-hd.gif"
 
 excludeDirList="/cache,/blocks,/content"
 
-acceptDomainList="localhost,www.economist.com,media.economist.com"
+acceptDomainList="localhost,www.economist.com,media.economist.com,cdn.static-economist.com"
 
 wget --no-check-certificate --restrict-file-names="windows,nocontrol" --timeout=${timeout} --execute robots=off --user-agent="${userAgent}" --html-extension --convert-links --timestamping --recursive --page-requisites --span-hosts --level=${maxLevel} --wait=${waitTime} --random-wait --domains="${acceptDomainList}" --reject "${rejectList}" --exclude-directories=${excludeDirList} "http://localhost/economist-weekly.html"
